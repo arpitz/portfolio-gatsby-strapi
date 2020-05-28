ded7cfa344a8363c90d5f4767554b06abf04ec5d
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-05-28 18:40:43 +0530

    Set up the hero

--- portfolio-gatsby-strapi/src/components/Hero.js
+++ portfolio-gatsby-strapi/src/components/Hero.js
@@ -5,8 +5,35 @@ import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
 
+const query = graphql`
+  {
+    file(relativePath: {eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
 const Hero = () => {
-  return <h2>hero component</h2>
+  const { file: { childImageSharp: { fluid } } } = useStaticQuery(query);
+
+  return <header className="hero">
+    <div className="section-center hero-center">
+      <article className="hero-info">
+        <div className="underline"></div>
+        <h1>i'm arpit</h1>
+        <h4>freelance web and mobile UI/UX designer</h4>
+        <Link to="/contact" className="btn">
+          contact me
+          </Link>
+        <SocialLinks />
+      </article>
+      <Image fluid={fluid} className="hero-img" />
+    </div>
+  </header>
 }
 
 export default Hero
--- portfolio-gatsby-strapi/src/pages/index.js
+++ portfolio-gatsby-strapi/src/pages/index.js
@@ -6,7 +6,10 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
+
 export default () => {
-  return <Layout></Layout>
+  return <Layout>
+    <Hero />
+  </Layout>
 }
 // ...GatsbyImageSharpFluid
